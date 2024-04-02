81c9f743604e180d950244e60f7fc767993c9ee3
Bakul Kandpal <113046581+bakulkandpal@users.noreply.github.com>
2024-04-02 10:32:58 +0530

    update plots

--- Main.py
+++ Main.py
@@ -20,6 +20,7 @@ time_range2 = (20, 22)  # Second set of hours, in between which, E-buses arrive.
 
 transformer_capacity = 8500 # Assumed capacity of distribution transformer in kVA.
 station_location_grid=15  # The node at which the charging station is located in the power grid.
+network_plots=False  # Input plot=True if grid voltages are to be shown, else False.
 
 ####### Battery State of Charge Parameters
 
@@ -32,11 +33,10 @@ battery_capacity = 240  # Battery capacity of single E-Bus in kWh.
 pv_size = 2000  # Size of PV in kW.
 
 
-
 ####### Calling External Functions
 charging_requirements, feeder_load_15min = feeder_data_class(num_buses, soc_lower, soc_upper, battery_capacity, charger_power, num_chargers, day_of_year, time_range1, time_range2).plots_15_mins() 
 indices_feeder, indices_ebus, day_indices, day_indices_ebus = grid_problems_15min(charging_requirements, feeder_load_15min, transformer_capacity)
-voltage, current = perform_load_flow(station_location_grid, plot=False)  # Input plot=True if grid voltages are to be shown.
+voltage, current = perform_load_flow(station_location_grid, plot=network_plots)  # Input plot=True if grid voltages are to be shown.
 demand_response = demand_response_class(charging_requirements, feeder_load_15min, pv_size, day_of_year)
 pv_generation_15min, daily_net_load, day_pv_hourly = demand_response.optimized_asset()
 charging_data, hourly_data = generate_charging_data_15min(num_buses, soc_lower, soc_upper, battery_capacity, charger_power, time_range1, time_range2)
